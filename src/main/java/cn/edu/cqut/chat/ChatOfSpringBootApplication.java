e7e7cc8041c5719cebe2ecb6b7f186aa3032d87a
GmwEnterprise <909498022@qq.com>
2019-02-19 20:04:57 +0800

    2019年2月19日 20:04:51

---
+++ src/main/java/cn/edu/cqut/chat/ChatOfSpringBootApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.cqut.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatOfSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChatOfSpringBootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/cn/edu/cqut/chat/ChatOfSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.edu.cqut.chat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatOfSpringBootApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
